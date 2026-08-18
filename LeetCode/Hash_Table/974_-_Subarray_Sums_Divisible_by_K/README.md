fd2d8bba309daf7d18f375d3ad9d8056afbfba81
Krusha Navadiya <9b66krusha.kesh@gmail.com>
2026-08-18 12:43:51 +0530

    Sync 974 - Subarray Sums Divisible by K (LeetCode)

---
+++ LeetCode/Hash_Table/974_-_Subarray_Sums_Divisible_by_K/README.md
@@ -0,0 +1,6 @@
+# 974 - Subarray Sums Divisible by K
+
+**Difficulty:** Medium
+
+## Approach / Notes
+No notes provided.
---
+++ LeetCode/Hash_Table/974_-_Subarray_Sums_Divisible_by_K/solution.cpp
@@ -0,0 +1,21 @@
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n=nums.size();
+
+        int sum=0;
+        int ans=0;
+
+        unordered_map<int, int> fq;
+        fq[0]=1;
+
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            int rem=(sum%k+k)%k;
+            ans+=fq[rem];
+            fq[rem]++;
+        }
+
+        return ans;
+    }
+};
+
