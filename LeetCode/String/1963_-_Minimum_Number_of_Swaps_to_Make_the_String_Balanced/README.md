a959ca43ecc80202963b3adea1c4c904ade41bf7
Krusha Navadiya <9b66krusha.kesh@gmail.com>
2026-08-20 20:09:35 +0530

    Sync 1963 - Minimum Number of Swaps to Make the String Balanced (LeetCode)

---
+++ LeetCode/String/1963_-_Minimum_Number_of_Swaps_to_Make_the_String_Balanced/README.md
@@ -0,0 +1,6 @@
+# 1963 - Minimum Number of Swaps to Make the String Balanced
+
+**Difficulty:** Medium
+
+## Approach / Notes
+No notes provided.
---
+++ LeetCode/String/1963_-_Minimum_Number_of_Swaps_to_Make_the_String_Balanced/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int count=0;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='[') count++;
+        }
+            else{
+                if(count>0) count--;
+            }
+    }
+
+        return (count+1)/2;
+};
+
