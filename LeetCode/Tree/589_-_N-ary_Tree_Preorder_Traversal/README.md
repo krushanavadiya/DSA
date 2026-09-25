c370cb4936d12bbbcc6ecebe12c14809ca659b24
Krusha Navadiya <9b66krusha.kesh@gmail.com>
2026-09-25 08:40:50 +0530

    Sync 589 - N-ary Tree Preorder Traversal (LeetCode)

---
+++ LeetCode/Tree/589_-_N-ary_Tree_Preorder_Traversal/README.md
@@ -0,0 +1,6 @@
+# 589 - N-ary Tree Preorder Traversal
+
+**Difficulty:** Easy
+
+## Approach / Notes
+No notes provided.
---
+++ LeetCode/Tree/589_-_N-ary_Tree_Preorder_Traversal/solution.cpp
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> ans;
+        traverse(root, ans);
+        return ans;
+    }
+
+    void traverse(Node* root, vector<int>& ans){
+        if(root==nullptr) return;
+
+        int n=root->children.size();
+
+        ans.push_back(root->val);
+
+        for(int i=0; i<n; i++){
+            traverse(root->children[i], ans);
+        }
+    }
+};
